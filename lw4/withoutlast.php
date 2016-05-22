028364b95751ff5192711d908b76f36b660af9c9
chosti34 <chosti34@gmail.com>
2016-05-22 23:43:24 +0300

    format text
    just added 2 spaces

--- lw4/withoutlast.php
+++ lw4/withoutlast.php
@@ -1,12 +1,12 @@
 <?php
-  require_once 'include/common.inc.php';
+    require_once 'include/common.inc.php';
   
-  $str = getParamFromGet('message');
-  if (empty($str))
-  {
-      echo 'Введите строку';
-  }
-  else
-  {
-      echo withoutlast($str);
-  }
+    $str = getParamFromGet('message');
+    if (empty($str))
+    {
+        echo 'Введите строку';
+    }
+    else
+    {
+        echo withoutlast($str);
+    }
