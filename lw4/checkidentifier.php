b2170f40287ba8b6bcf6d6da4cdb9117d02d5994
chosti34 <chosti34@gmail.com>
2016-05-22 23:40:50 +0300

    update

--- lw4/checkidentifier.php
+++ lw4/checkidentifier.php
@@ -1,12 +1,12 @@
 <?php
-  require_once '/include/common.inc.php';
+    require_once '/include/common.inc.php';
   
-  $str = getParamFromGet('id');
-  if (empty($str))
-  {
-      echo 'Введите строку';
-  }
-  else
-  {
-      echo checkIdentifier($str);
-  }
+    $str = getParamFromGet('id');
+    if (empty($str))
+    {
+        echo 'Введите строку';
+    }
+    else
+    {
+        echo checkIdentifier($str);
+    }
