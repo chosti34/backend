2a22c01ceaa40691e2401262a43c9d6057373ccb
chosti34 <chosti34@gmail.com>
2016-05-31 20:27:24 +0300

    Add files via upload

---
+++ lw4/check_identifier.php
@@ -0,0 +1,19 @@
+<?php
+    require_once '/include/common.inc.php';
+  
+    $str = getParamFromGet('id');
+    if (empty($str))
+    {
+        echo 'Введите строку';
+    }
+    else
+    {
+        if (checkIdentifier($str))
+        {
+            echo 'yes';
+        }
+        else
+        {
+            echo 'no';
+        }
+    }
